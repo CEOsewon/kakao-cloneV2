35a29bd164c5a86c7f86d28041df5ce064f5b347
CEOsewon <59468796+CEOsewon@users.noreply.github.com>
2020-01-30 21:56:24 +0900

    Finished (kokoa V2)
    01/30
    21:56

--- javascript/time.js
+++ javascript/time.js
@@ -2,11 +2,11 @@ const timeStamp = document.querySelector(".status-bar-timestamp");
 
 function getTime() {
   const currentTime = new Date();
-  const hour = currentTime.getHours("hour");
-  const minute = currentTime.getMinutes("minute");
-  const second = currentTime.getSeconds("second");
-  timeStamp.innerText = `${hour}:${minute}:${
-    second > 9 ? second : `0${second}`
+  const hour = currentTime.getHours();
+  const minute = currentTime.getMinutes();
+  const second = currentTime.getSeconds();
+  timeStamp.innerText = `${hour}:${minute >= 10 ? minute : `0${minute}`}:${
+    second >= 10 ? second : `0${second}`
   }`;
 }
 
